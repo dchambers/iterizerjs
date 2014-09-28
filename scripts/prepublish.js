3c03174a5378ef2c6a5bddb113f012f2c45f2e3b
Dominic Chambers <dominic.chambers@gmail.com>
2014-09-28 20:33:32 +0100

    Made a change so that 'dist/iterizer.js' can now be used in the way described in 'README.md'.

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -8,7 +8,7 @@ mkdirp('dist/jasmine-2.0.0', function(err) {
 		console.error(err);
 	}
 	else {
-		createBundle('dist/iterizer.js', ['./lib/iterizer.js']);
+		createBundle('dist/iterizer.js', ['./lib/global-iterizer.js']);
 		createBundle('dist/iterizer-spec-tests.js', ['./spec/tests/tests.spec.js', './spec/project-euler/project-euler.spec.js']);
 
 		ncp('node_modules/jasmine-node/node_modules/jasmine-reporters/lib/jasmine-2.0.0', 'dist/jasmine-2.0.0', function(err) {
