de5162c18ec75ad50ad7af98e827c212807da256
Dominic Chambers <dominic.chambers@gmail.com>
2014-10-07 23:35:47 +0100

    Re-enabled problem 10 now that the Project Euler tests run in their own suite.

--- spec/project-euler/problem10.js
+++ spec/project-euler/problem10.js
@@ -5,9 +5,8 @@ describe('Project Euler Problem #10', function() {
 		expect(primes().limit(lessThan(10)).sum()).toBe(17);
 	});
 
-	// Note: commented out because it adds about 5 seconds to the test time
-//	it('displays the sum of all primes below 2000000', function() {
-//		expect(primes().limit(lessThan(2000000)).sum()).toBe(142913828922);
-//	});
+	it('displays the sum of all primes below 2000000', function() {
+		expect(primes().limit(lessThan(2000000)).sum()).toBe(142913828922);
+	});
 });
 
