02dec41a6940ee894bc14c665b3d9a978b1365c7
Dominic Chambers <dominic.chambers@gmail.com>
2014-10-08 20:19:42 +0100

    Changed 'npm test' so that it now also runs the Karma tests, and modified the 'npm run test-browser' command so that it keeps the tests running in the background, for those that want that functionality. Also modified the jasmine tests so that they will now fail if an error occurs at any point, preventing the tests from running.

--- spec/node-spec-runner.js
+++ spec/node-spec-runner.js
@@ -1,12 +1,29 @@
 var jn = require('jasmine-node');
+var karmaServer = require('karma').server;
+var path = require('path');
+
 console.log('Test Install:');
-jn.run({specFolders:['./spec/test-install'], onComplete:function() {
+
+// TODO: find out how 'node-jasmine' allows you to get test result information so we can return a non-zero exit code
+jn.run({specFolders:['./spec/test-install'], captureExceptions:true, onComplete:function() {
 	console.log('');
 	console.log('Spec Tests:');
-	jn.run({specFolders:['./spec/test'], onComplete:function() {
+
+	jn.run({specFolders:['./spec/test'], captureExceptions:true, onComplete:function() {
 		console.log('');
 		console.log('Project Euler:');
-		jn.run({specFolders:['./spec/project-euler']});
+
+		jn.run({specFolders:['./spec/project-euler'], captureExceptions:true, onComplete:function() {
+			console.log('');
+			console.log('Browser Tests:');
+
+			setTimeout(function() {
+				karmaServer.start({configFile: path.resolve('karma-conf.js'), singleRun:true}, function(exitCode) {
+					console.log('Karma has exited with ' + exitCode);
+					process.exit(exitCode);
+				});
+			}, 0)
+		}});
 	}});
 }});
 
